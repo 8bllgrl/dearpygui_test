eae5ebe0913a90a2d515093f1660d3ea8e04f8d6
Sage Belknap <sbelknap@cmc.com>
2024-10-24 13:43:36 -0600

    escape key brings ui back.

--- imguipackage1/application.py
+++ imguipackage1/application.py
@@ -8,17 +8,35 @@ class Application:
         self.theme = Theme()
         self.viewport = Viewport(800, 600)
         self.imgui_controller = ImGuiController()
+        self.ui_visible = True
+        self.main_window_id = None
 
     def run(self):
         dpg.create_context()
         self.theme.setup_theme()
 
-        self.imgui_controller.create_main_window()
+        self.main_window_id = self.imgui_controller.create_main_window()
+        print(f"Main window ID: {self.main_window_id}")
 
         self.viewport.create_viewport()
         self.viewport.center_on_monitor()
+
+        with dpg.handler_registry():
+            dpg.add_key_press_handler(dpg.mvKey_A, callback=self.on_a_press)
+            dpg.add_key_press_handler(dpg.mvKey_Escape, callback=self.on_escape_press)
 
         dpg.setup_dearpygui()
         dpg.show_viewport()
         dpg.start_dearpygui()
         dpg.destroy_context()
+
+    def on_a_press(self, sender, app_data):
+        print("A pressed")
+
+    def on_escape_press(self, sender, app_data):
+        self.ui_visible = not self.ui_visible
+        if self.ui_visible:
+            dpg.show_item(self.main_window_id)
+        else:
+            dpg.hide_item(self.main_window_id)
+
--- imguipackage1/imgui/imgui_controller.py
+++ imguipackage1/imgui/imgui_controller.py
@@ -1,14 +1,13 @@
 import dearpygui.dearpygui as dpg
 from imguipackage1.imgui.tab_manager import TabManager
 
-
 class ImGuiController:
     def __init__(self):
         self.tab_manager = TabManager()
 
     def create_main_window(self):
-        with dpg.window(label="Main Window", width=400, height=300):
-
+        window_id = "main_window"
+        with dpg.window(label="Main Window", width=400, height=300, tag=window_id):
             with dpg.tab_bar(label="Settings"):
                 self.tab_manager.create_tabs()
-
+        return window_id
