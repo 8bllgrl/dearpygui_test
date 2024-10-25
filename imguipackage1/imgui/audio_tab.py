6762dde6f78f46cf4b6f45c45f16612fb9f119db
Sage Belknap <sbelknap@cmc.com>
2024-10-25 08:47:27 -0600

    volume simplify

--- imguipackage1/imgui/audio_tab.py
+++ imguipackage1/imgui/audio_tab.py
@@ -5,23 +5,13 @@ from imguipackage1.theme.colors import Colors
 class AudioTab:
     def __init__(self, audio_manager: AudioSettings):
         self.audio_manager = audio_manager
-
-        self.master_volume_control = VolumeControl(
-            self.audio_manager.master_volume,
-            self.update_master_volume,
-            "Master Volume"
-        )
-        self.music_volume_control = VolumeControl(
-            self.audio_manager.music_volume,
-            self.update_music_volume,
-            "Music Volume"
-        )
+        self.volume_controls = []
 
     def create(self):
         with dpg.tab(label="Audio"):
             self.add_audio_settings_text()
-            self.master_volume_control.create()
-            self.music_volume_control.create()
+            self.add_volume_control("Master Volume", self.audio_manager.master_volume, self.audio_manager.update_master_volume)
+            self.add_volume_control("Music Volume", self.audio_manager.music_volume, self.audio_manager.update_music_volume)
             self.add_reset_button()
 
     def add_audio_settings_text(self):
@@ -31,15 +21,19 @@ class AudioTab:
     def add_reset_button(self):
         dpg.add_button(label="Reset Audio Settings", callback=self.reset_audio_settings)
 
-    def update_master_volume(self, sender, app_data):
-        self.audio_manager.update_master_volume(app_data)
-        dpg.set_value(self.master_volume_control.volume_id, self.audio_manager.master_volume)
+    def add_volume_control(self, label, initial_value, update_callback):
+        volume_control = VolumeControl(initial_value, lambda sender, app_data: self.update_volume(update_callback, app_data, volume_control), label)
+        volume_control.create()
+        self.volume_controls.append(volume_control)
 
-    def update_music_volume(self, sender, app_data):
-        self.audio_manager.update_music_volume(app_data)
-        dpg.set_value(self.music_volume_control.volume_id, self.audio_manager.music_volume)
+    def update_volume(self, update_callback, app_data, volume_control):
+        update_callback(app_data)
+        dpg.set_value(volume_control.volume_id, app_data)
 
     def reset_audio_settings(self, sender):
         self.audio_manager.reset_audio_settings()
-        dpg.set_value(self.master_volume_control.volume_id, self.audio_manager.master_volume)
-        dpg.set_value(self.music_volume_control.volume_id, self.audio_manager.music_volume)
+        for control in self.volume_controls:
+            if control.label == "Master Volume":
+                dpg.set_value(control.volume_id, self.audio_manager.master_volume)
+            elif control.label == "Music Volume":
+                dpg.set_value(control.volume_id, self.audio_manager.music_volume)
